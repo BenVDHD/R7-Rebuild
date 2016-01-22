da7fe0aa9099bde0b3121ba0d30d24048c44008f
Ben Rusnac <benrusnac@gmail.com>
2016-01-21 20:04:13 -0500

    1/21/16 update

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <link rel="stylesheet" href="styles/main.css" type="text/css"></link>
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'></link>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no, maximum-scale=1.0, minimum-scale=1.0"></meta>
+  <!-- <meta name="viewport" content="user-scalable = yes"> -->
+  <script type="text/javascript" src="script/jquery.js"></script>
+</head>
+
+<body>
+  <script src="script/greeting.js"> </script>
+  <nav>
+    <a href="location.html">
+      <div style="width: 12.5vh; height: 100%; background-image: url('img/icon/icon_menu.png'); background-size: contain; float: left;"></div>
+    </a>
+    <a href="index.htmlgi">
+      <div style=" margin: 0 auto; width: 12.5vh; height: 100%; background-image: url('img/icon/logo-gray.png'); background-size: contain;"></div>
+    </a>
+  </nav>
