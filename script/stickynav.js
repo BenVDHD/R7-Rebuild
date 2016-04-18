5b4d1c390c71bd79daa5eef52b186f66a2597210
Ben Rusnac <ben@vdhd.net>
2016-04-18 14:11:18 -0400

    4/18/16

---
+++ script/stickynav.js
@@ -0,0 +1,9 @@
+$(window).scroll(function (e) {
+      $el = $('nav');
+      if ($(this).scrollTop() > window.innerHeight && $el.css('position') != 'fixed') {
+          $('nav').css({ 'position': 'fixed', 'top': '0', 'left':'0', 'right':'0'});
+        }
+        if ($(this).scrollTop() < window.innerHeight && $el.css('position') == 'fixed') {
+          $('nav').css({ 'position': 'absolute', 'top': '100vh', 'left':'', 'right':'' });
+        }
+      });
